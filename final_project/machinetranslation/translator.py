b45e735f01953c4043f6921efe97a3a662c08824
https://github.com/jtaflan/xzceb-flask_eng_fr <jtaflan@att.net>
2023-03-11 15:23:41 -0500

    Final changes

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -6,23 +6,23 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-apikey = os.environ['_OBZyB28gwASdAr-0ttdyJ-ywnzvhDuOZ8vnpVL71jL2']
-url = os.environ['https://api.us-east.language-translator.watson.cloud.ibm.com/instances/2148bef1-7213-43ed-b49e-56be883fdb90']
+apikey = os.environ['apikey']
+url = os.environ['url']
 
 
-authenticator = IAMAuthenticator('api')
-language_translator = LanguageTranslatorV3( version='2018-05-01',
-    authenticator=authenticator)
+authenticator = IAMAuthenticator(apikey)
+language_translator = LanguageTranslatorV3(version='2018-05-01', authenticator=authenticator)
 
-language_translator.set_service_url('url')
-# pylint: disable=missing-function-docstring
+language_translator.set_service_url(url)
+
+
 def english_to_french(english_text):
     translation = language_translator.translate(text=english_text, model_id='en-fr').get_result()
     french_text = translation['translations'][0]['translation']
-    return french_text # r\n
+    return french_text
+
 
 def french_to_english(french_text):
     translation = language_translator.translate(text=french_text, model_id='fr-en').get_result()
     english_text = translation['translations'][0]['translation']
-    return english_text # r\n
-    # pylint: disable=C0304
+    return english_text
